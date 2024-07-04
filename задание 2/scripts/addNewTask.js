113e6d6b85d508b34da4d248651f1e14c47b259b
Vlada Davydova <davudovavlada@gmail.com>
2024-07-05 03:56:11 +0500

    отлетело сохранение

--- задание 2/scripts/addNewTask.js
+++ задание 2/scripts/addNewTask.js
@@ -1,37 +1,36 @@
-document.getElementById('save-btn').addEventListener("click", () => {
+document.getElementById('save-btn').addEventListener('submit', event => {
+    event.preventDefault()
     let numberTask = document.getElementById('number-task').value,
         workerName = document.getElementById('worker-name').value,
         masterName = document.getElementById('master-name').value,
         departTask = document.getElementById('depart-task').value
 
     let taskObj = {
-            idTask: 11111111,
-            numberTask: numberTask,
-            departTask: departTask,
-            masterName: masterName,
-            workerName: workerName,
-            dateIssue: new Date(),
-            dateAccept: null
-        }
-        CreateDataFile('listTask')
-
-    function CreateDataFile(fileName) {
-        fetch(`../json/${fileName}.json`)
-            .then(response => response.json())
-            .then(data => {
-                const updatedData = {...data, ...taskObj};
-                const json = JSON.stringify(updatedData, null, 2);
-                const blob = new Blob([json], {type: 'application/json'});
-                const url = URL.createObjectURL(blob);
-
-                const a = document.createElement('a');
-                a.href = url;
-                a.download = `${fileName}.json`;
-                a.click();
-                URL.revokeObjectURL(url);
-            })
+        idTask: 11111111,
+        numberTask: numberTask,
+        departTask: departTask,
+        masterName: masterName,
+        workerName: workerName,
+        dateIssue: new Date(),
+        dateAccept: null
     }
 
-    history.back()
+
+    fetch('/json/listTask.json')
+        .then(response => response.json())
+        .then(data => {
+            const updatedData = {...data, ...taskObj};
+
+            const json = JSON.stringify(updatedData, null, 2);
+
+            const blob = new Blob([json], {type: 'application/json'});
+            const url = URL.createObjectURL(blob);
+
+            const a = document.createElement('a');
+            a.href = url;
+            a.download = `listTask.json`;
+            a.click();
+            URL.revokeObjectURL(url);
+        })
 })
 
